b3ccc48ae8b27523014d4d55173a8db69806114c
kevinwairi <254wairi@gmail.com>
2018-09-25 16:49:57 +0300

    added class Quote

---
+++ src/app/quote.ts
@@ -0,0 +1,3 @@
+export class Quotes {
+    constructor(public id:number, public name:string, public quoteWords: string, public author: string, public upvote:number, public downvote:number,public date:Date){}
+}
