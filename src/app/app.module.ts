1f726cf926e1ba740a0ad169eac73380eb77f27c
kevinwairi <254wairi@gmail.com>
2018-09-25 17:01:04 +0300

    import form module from angular forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteNavComponent } from './quote-nav/quote-nav.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuoteInfoComponent } from './quote-info/quote-info.component';
+import { TimePipe } from './time.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent,
+    QuoteNavComponent,
+    QuoteFormComponent,
+    QuoteInfoComponent,
+    TimePipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
