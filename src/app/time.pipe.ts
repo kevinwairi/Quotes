6ef19151cbc7b570d1ab1d5b969c3834129d339b
kevinwairi <254wairi@gmail.com>
2018-09-25 16:50:35 +0300

    add pipe time for countdown

---
+++ src/app/time.pipe.ts
@@ -0,0 +1,23 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'time'
+})
+export class TimePipe implements PipeTransform {
+
+  transform(value: any): number {
+    let today: Date = new Date(); //   get current date and time
+    let todayWithNoTime: any = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+    var dateDifference = Math.abs(value - todayWithNoTime ); //   return value in milliseconds
+    const secondsInADay = 86400; //  60 s * 60 m in an hour *24 hours
+    var dateDifferenceSeconds =  dateDifference * 0.001; //  converts to seconds
+    var dateCounter = dateDifferenceSeconds / secondsInADay;
+
+    if (dateCounter >= 1) {
+        return dateCounter;
+    } else {
+        return 0;
+    }
+  }
+
+}
